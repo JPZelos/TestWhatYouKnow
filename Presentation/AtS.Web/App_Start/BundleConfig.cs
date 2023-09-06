7e450e88e2977b321dfbf3637d76d7c89e6485dd
JPZelos <i.p.zelos@gmail.com>
2023-09-06 11:32:30 +0300

    intial Quiz Layout

--- Presentation/AtS.Web/App_Start/BundleConfig.cs
+++ Presentation/AtS.Web/App_Start/BundleConfig.cs
@@ -1,14 +1,51 @@
-﻿using System.Web.Optimization;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Optimization;
 
 namespace TWYK.Web
 {
+    /// <summary>
+    /// see: https://stackoverflow.com/questions/19323409/asp-net-mvc-bundle-config-order
+    /// </summary>
+    public class NonOrderingBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<FileInfo> OrderFiles(BundleContext context, IEnumerable<FileInfo> files)
+        {
+            return files;
+        }
+
+        #region Implementation of IBundleOrderer
+
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files) {
+            return files;
+        }
+
+        #endregion
+    }
+
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Content/js/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Content/js/jquery-{version}.js"));
+
+            //var mitsos = (new Bundle("~/bundles/mitsos").Include(
+            //    "~/Content/assets/js/Bootstrap/bootstrap.min.js",
+            //    "~/Content/assets/js/jQuery/jquery-3.6.3.min.js",
+            //    "~/Content/assets/js/result.js",
+            //    "~/Content/assets/js/custom.js"
+            //));
+
+            var mitsos = (new Bundle("~/bundles/mitsos").Include(
+                "~/Content/assets/js/Bootstrap/bootstrap.min.js",
+                "~/Content/assets/js/jQuery/jquery-3.6.3.min.js"
+            ));
+
+            //see: https://stackoverflow.com/questions/19323409/asp-net-mvc-bundle-config-order
+            mitsos.Orderer = new NonOrderingBundleOrderer();
+            bundles.Add(mitsos);
 
             bundles.Add(new ScriptBundle("~/bundles/ajaxcart").Include(
                         "~/Content/js/custom/public.common.js",
@@ -22,11 +59,22 @@ namespace TWYK.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Content/js/modernizr-*"));
 
+
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/css/styles.css",
+            //          "~/Content/css/cart2.css",
+            //          "~/Content/css/shoping-cart.css",
+            //          "~/Content/css/site.css"));
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/css/styles.css",
-                      "~/Content/css/cart2.css",
-                      "~/Content/css/shoping-cart.css",
-                      "~/Content/css/site.css"));
+                "~/Content/assets/css/Bootstrap/bootstrap.min.css",
+                "~/Content/assets/css/style.css",
+                "~/Content/assets/css/responsive.css",
+                "~/Content/assets/css/animation.css",
+                "~/Content/assets/css/result_style.css"));
+
         }
     }
 }
