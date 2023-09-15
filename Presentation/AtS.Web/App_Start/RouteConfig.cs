d9ca66c7c16cec7922bd547e095f3e6fa2b43593
JPZelos <i.p.zelos@gmail.com>
2023-09-15 14:54:28 +0300

    Student Results styling

--- Presentation/AtS.Web/App_Start/RouteConfig.cs
+++ Presentation/AtS.Web/App_Start/RouteConfig.cs
@@ -10,105 +10,97 @@ namespace TWYK.Web
 
             //define this routes to use in UI views (in case if you want to customize some of them later)
             routes.MapRoute(
-                name: "Product",
-                url: "catalog/product/{productId}",
+                "Product",
+                "catalog/product/{productId}",
                 new { controller = "catalog", action = "ProductDetails" });
 
             //twyn
             routes.MapRoute(
-                name: "Chapter",
-                url: "catalog/chapter/{chapterId}",
+                "Chapter",
+                "catalog/chapter/{chapterId}",
                 new { controller = "catalog", action = "ChaptertDetails" });
 
             //twyn
             routes.MapRoute(
-                name: "DoTest",
-                url: "catalog/DoTest/{chapterId}",
+                "DoTest",
+                "catalog/DoTest/{chapterId}",
                 new { controller = "answer", action = "DoTest" });
-
 
             //twyn
             routes.MapRoute(
-                name: "ActionDenied",
-                url: "catalog/ActionDenied",
+                "ActionDenied",
+                "catalog/ActionDenied",
                 new { controller = "Admin", action = "ActionDenied" });
 
             //twyn
             routes.MapRoute(
-                name: "AdminUsers",
-                url: "admin/Users/",
+                "AdminUsers",
+                "admin/Users/",
                 new { controller = "Admin", action = "AdminUsers" }
             );
-            
+
             //twyn
             routes.MapRoute(
-                name: "TeacherUsers",
-                url: "admin/TeacherUsers",
+                "TeacherUsers",
+                "admin/TeacherUsers",
                 new { controller = "Admin", action = "TeacherUsers" }
             );
 
             //twyn
             routes.MapRoute(
-                name: "TeacherTopics",
-                url: "admin/TeacherTopics",
+                "TeacherTopics",
+                "admin/TeacherTopics",
                 new { controller = "Admin", action = "Topics" }
             );
 
             //twyn
             routes.MapRoute(
-                name: "Chapters",
-                url: "admin/Chapters",
+                "Chapters",
+                "admin/Chapters",
                 new { controller = "Admin", action = "Chapters" }
             );
 
-
             //twyn
             routes.MapRoute(
-                name: "Questions",
-                url: "admin/Questions/{chapterId}",
+                "Questions",
+                "admin/Questions/{chapterId}",
                 new { controller = "Admin", action = "Questions" }
             );
 
-
             //admin home page
             routes.MapRoute(
-                name: "AdminHomePage",
-                url: "admin/",
+                "AdminHomePage",
+                "admin/",
                 new { controller = "Admin", action = "Index" }
             );
 
-
             routes.MapRoute(
-                name: "Login",
-                url: "login/",
+                "Login",
+                "login/",
                 new { controller = "Account", action = "Login" });
-
 
             //page not found
             routes.MapRoute(
-                name: "PageNotFound",
-                url: "page-not-found",
+                "PageNotFound",
+                "page-not-found",
                 new { controller = "Common", action = "PageNotFound" });
 
-
             routes.MapRoute(
-                name: "DatabaseInfo",
-                url: "page-not-found",
+                "DatabaseInfo",
+                "page-not-found",
                 new { controller = "Common", action = "DatabaseInfo" });
-
 
             //home page
             routes.MapRoute(
-                name: "HomePage",
-                url: "",
-                new {controller = "Home", action = "Index"}
+                "HomePage",
+                "",
+                new { controller = "Home", action = "Index" }
             );
-
 
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
-                new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
